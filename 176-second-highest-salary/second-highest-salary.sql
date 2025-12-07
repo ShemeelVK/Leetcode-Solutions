81ea2593c81c2be8fe4f9edc1b5cb094e7bfae05
Shemeel vk <157100223+ShemeelVK@users.noreply.github.com>
2025-12-07 12:57:21 +0530

    Time: 324 ms (98.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+select 
+case when  (select count(salary)from employee) <2
+         then null
+        else(
+            select max(salary)from employee 
+            where salary<(select max(salary)from employee)) 
+
+        end as  SecondHighestSalary
+        
